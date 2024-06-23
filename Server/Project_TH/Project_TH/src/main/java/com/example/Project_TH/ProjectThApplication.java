bb04e31464f31294e1d5089c0be78a87890c3ebc
La Hoang Phuc <phuclaits03@gmail.com>
2024-06-23 14:05:48 +0700

    started project

---
+++ Server/Project_TH/Project_TH/src/main/java/com/example/Project_TH/ProjectThApplication.java
@@ -0,0 +1,13 @@
+package com.example.Project_TH;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectThApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectThApplication.class, args);
+	}
+
+}
